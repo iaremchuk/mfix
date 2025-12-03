57d7b7537898f40aef0bdbb0cf419484db4fb793
iaremchuk <66682491+iaremchuk@users.noreply.github.com>
2025-12-03 11:27:48 +0000

    Added viewButton to home page and vhange some styling (#17)

--- src/components/view-projects/ViewProjects.jsx
+++ src/components/view-projects/ViewProjects.jsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material'
 import AppCarousel from '~/components/app-carousel/AppCarousel'
 import ContentContainer from '~/containers/content-container/ContentContainer'
+import ViewButton from '~/components/view-button/ViewButton'
 import { styles } from '~/components/view-projects/ViewProjects.styles.js'
 
 const images = import.meta.glob('~/assets/projects/**/*.jpg', { eager: true })
@@ -13,20 +14,21 @@ const projectImages = Object.entries(images).map(([path, mod], index) => ({
 const ViewProjects = () => {
   return (
     <ContentContainer>
+      <ViewButton sx={{ marginTop: '50px' }} />
       <Box sx={styles.viewProjectsContainer} position="relative">
         <AppCarousel
-          width='100%'
-          height='100%'
+          width="100%"
+          height="100%"
           interval={2000}
           showButtons={false}
         >
           {projectImages.map((image, index) => (
             <Box
               key={index}
-              component='img'
+              component="img"
               src={image.src}
               alt={image.alt}
-              loading='lazy'
+              loading="lazy"
               sx={styles.viewProjectsImage}
             />
           ))}
